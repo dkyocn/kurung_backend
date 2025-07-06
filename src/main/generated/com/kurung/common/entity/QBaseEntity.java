dab962d7126223503fc7a5277251ae77c275a1e0
dkyocn <gioconda77@daum.net>
2025-07-06 11:28:36 +0900

    [CHORE] swagger, restDocs 설정 추가

---
+++ src/main/generated/com/kurung/common/entity/QBaseEntity.java
@@ -0,0 +1,39 @@
+package com.kurung.common.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QBaseEntity is a Querydsl query type for BaseEntity
+ */
+@Generated("com.querydsl.codegen.DefaultSupertypeSerializer")
+public class QBaseEntity extends EntityPathBase<BaseEntity> {
+
+    private static final long serialVersionUID = 445271223L;
+
+    public static final QBaseEntity baseEntity = new QBaseEntity("baseEntity");
+
+    public final DatePath<java.sql.Date> createDate = createDate("createDate", java.sql.Date.class);
+
+    public final DatePath<java.sql.Date> modifyDate = createDate("modifyDate", java.sql.Date.class);
+
+    public QBaseEntity(String variable) {
+        super(BaseEntity.class, forVariable(variable));
+    }
+
+    public QBaseEntity(Path<? extends BaseEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QBaseEntity(PathMetadata metadata) {
+        super(BaseEntity.class, metadata);
+    }
+
+}
+
