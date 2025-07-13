623e858d8df826aab9b451a6359a370659b4af3b
chcyij9914 <chcyij9914@naver.com>
2025-07-13 19:36:58 +0900

    [FIX] 코드 수정

--- src/main/java/com/kurung/missions/service/MissionsServiceImpl.java
+++ src/main/java/com/kurung/missions/service/MissionsServiceImpl.java
@@ -1,32 +1,18 @@
 package com.kurung.missions.service;
 
-import static com.kurung.missions.entity.QMissionsEntity.missionsEntity;
-import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
-
 import com.kurung.common.enumeration.CustomHttpStatus;
-import com.kurung.common.enumeration.HealthType;
 import com.kurung.common.exception.CustomIllegalArgumentException;
 import com.kurung.missions.dto.MissionsDTO;
 import com.kurung.missions.entity.MissionsEntity;
 import com.kurung.missions.repository.MissionsRepository;
-import com.kurung.missions.repository.MissionsRepositorySupport;
 import com.kurung.user.dto.UserDTO;
-import com.kurung.user.entity.UserEntity;
-import com.kurung.user.repository.UserRepository;
 import com.kurung.user.service.UserService;
-import jakarta.persistence.Table;
-import java.sql.Date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -59,12 +45,6 @@ public class MissionsServiceImpl implements MissionsService {
     LocalDate localDate = startOfDay.toLocalDate();
 
     List<MissionsEntity> missionList = missionsRepository.getMissionList(userUuid, localDate);
-
-//    return Optional.ofNullable(missionList)
-//        .orElse(Collections.emptyList())
-//        .stream()
-//        .map(MissionsDTO::new)
-//        .collect(Collectors.toList());
 
     return missionList.stream()
         .map(MissionsDTO::new)
