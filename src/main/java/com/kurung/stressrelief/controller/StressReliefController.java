648be15693656870743cb836653ff5003d12df5d
dkyocn <gioconda77@daum.net>
2025-07-15 17:09:42 +0900

    [FEAT] 스트레스 해소 페이지 조회

--- src/main/java/com/kurung/stressrelief/controller/StressReliefController.java
+++ src/main/java/com/kurung/stressrelief/controller/StressReliefController.java
@@ -9,9 +9,15 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -31,6 +37,14 @@ public class StressReliefController {
     @Parameter(name = "id", description = "스트레스 조회 아이디", example = "1")
     public ResponseEntity<StressReliefDTO> getStressReliefById(@PathVariable int id) {
         return new ResponseEntity<>(stressreliefService.getStressReliefById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/page")
+    @Operation(summary = "스트레스해소 페이지 조회", description = "스트레스해소 페이지 조회 시 사용하는 API")
+    @ApiResponse(responseCode = "200", description = "조회 성공", content = @io.swagger.v3.oas.annotations.media.Content(mediaType = "application/json"))
+    @Parameter(name = "keyword", description = "검색어", example = "호흡")
+    public ResponseEntity<Page<StressReliefDTO>>  getStressReliefByPage(Pageable pageable, @RequestParam(required = false) String keyword) {
+        return new ResponseEntity<>(stressreliefService.getStressReliefByPage(pageable, keyword), HttpStatus.OK);
     }
 }
 
--- src/main/java/com/kurung/stressrelief/repository/StressReliefRepositorySupportImpl.java
+++ src/main/java/com/kurung/stressrelief/repository/StressReliefRepositorySupportImpl.java
@@ -3,8 +3,14 @@ package com.kurung.stressrelief.repository;
 import static com.kurung.stressrelief.entity.QStressReliefEntity.stressReliefEntity;
 
 import com.kurung.stressrelief.entity.StressReliefEntity;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import io.micrometer.common.util.StringUtils;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 
@@ -21,5 +27,27 @@ public class StressReliefRepositorySupportImpl implements StressReliefRepository
                 .selectFrom(stressReliefEntity)
                 .where(stressReliefEntity.stressReliefId.eq(id))
                 .fetchOne();
+    }
+
+    @Override
+    public Page<StressReliefEntity> getStressReliefByPage(Pageable pageable, String keyword) {
+
+        List<StressReliefEntity> stressReliefEntities = jpaQueryFactory.selectFrom(stressReliefEntity)
+            .where(containKeyword(keyword))
+            .orderBy(stressReliefEntity.stressReliefTitle.desc())
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .fetch();
+
+        Long count = jpaQueryFactory.select(stressReliefEntity.count())
+            .from(stressReliefEntity)
+            .where(containKeyword(keyword))
+            .fetchOne();
+
+        return new PageImpl<>(stressReliefEntities, pageable, count);
+    }
+
+    BooleanExpression containKeyword(String keyword) {
+        return StringUtils.isNotBlank(keyword) ? stressReliefEntity.stressReliefTitle.contains(keyword) : null;
     }
 }
