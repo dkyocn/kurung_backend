fd9e62708fb78abfcc1c71e88a94fdecfa2401fc
dkyocn <gioconda77@daum.net>
2025-07-15 17:45:00 +0900

    [FEAT] 운동 페이지 조회

--- src/main/java/com/kurung/exercise/controller/ExerciseController.java
+++ src/main/java/com/kurung/exercise/controller/ExerciseController.java
@@ -16,10 +16,19 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -202,5 +211,11 @@ public class ExerciseController {
     return ResponseEntity.ok(result);
   }
 
-
+  @GetMapping("/page")
+  @Operation(summary = "운동 페이지 조회", description = "운동 페이지를 조회할 때 사용하는 API")
+  @ApiResponse(responseCode = "200", description = "조회 성공", content = @io.swagger.v3.oas.annotations.media.Content(mediaType = "application/json"))
+  @Parameter(name = "keyword", description = "검색어", example = "풀업")
+  public ResponseEntity<Page<ExerciseDTO>> getExercisePage(@RequestParam(required = false) String keyword, Pageable pageable) {
+    return new ResponseEntity<>(exerciseService.getExercisePage(keyword, pageable), HttpStatus.OK);
+  }
 }
--- src/main/java/com/kurung/exercise/repository/ExerciseRepositorySupportImpl.java
+++ src/main/java/com/kurung/exercise/repository/ExerciseRepositorySupportImpl.java
@@ -3,8 +3,14 @@ package com.kurung.exercise.repository;
 import static com.kurung.exercise.entity.QExerciseEntity.exerciseEntity;
 
 import com.kurung.exercise.entity.ExerciseEntity;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import io.micrometer.common.util.StringUtils;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -19,5 +25,27 @@ public class ExerciseRepositorySupportImpl implements ExerciseRepositorySupport 
         .selectFrom(exerciseEntity)
         .where(exerciseEntity.exerciseId.eq(id))
         .fetchOne();
+  }
+
+  @Override
+  public Page<ExerciseEntity> getExercisePage(String keyword, Pageable pageable) {
+
+    List<ExerciseEntity> exerciseEntities = queryFactory.selectFrom(exerciseEntity)
+        .where(containKeyword(keyword))
+        .orderBy(exerciseEntity.exerciseId.desc())
+        .offset(pageable.getOffset())
+        .limit(pageable.getPageSize())
+        .fetch();
+
+    Long count = queryFactory.select(exerciseEntity.count())
+        .from(exerciseEntity)
+        .where(containKeyword(keyword))
+        .fetchOne();
+
+    return new PageImpl<>(exerciseEntities, pageable, count);
+  }
+
+  BooleanExpression containKeyword(String keyword) {
+    return StringUtils.isNotBlank(keyword) ? exerciseEntity.exerciseName.contains(keyword) : null;
   }
 }
