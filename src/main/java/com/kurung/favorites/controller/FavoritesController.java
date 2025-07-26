47237c0631c94e68d40a97b6f8677d6291d5e875
dkyocn <gioconda77@daum.net>
2025-07-26 16:22:38 +0900

    [FIX] 즐겨찾기 uuid 변경 & 생성 리턴 변경

--- src/main/java/com/kurung/favorites/controller/FavoritesController.java
+++ src/main/java/com/kurung/favorites/controller/FavoritesController.java
@@ -1,6 +1,5 @@
 package com.kurung.favorites.controller;
 
-import com.kurung.common.enumeration.HealthType;
 import com.kurung.favorites.dto.FavoritesDTO;
 import com.kurung.favorites.enumeration.FavoritesType;
 import com.kurung.favorites.service.FavoritesService;
@@ -10,13 +9,19 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -33,17 +38,16 @@ public class FavoritesController {
       @ApiResponse(responseCode = "200", description = "저장 성공", content = @io.swagger.v3.oas.annotations.media.Content(mediaType = "application/json")),
       @ApiResponse(responseCode = "556", description = "저장 실패", content = @io.swagger.v3.oas.annotations.media.Content(mediaType = "application/json"))
   })
-  public ResponseEntity<HttpStatus> createFavorite(@RequestBody FavoritesDTO favoritesDTO) {
-    favoritesService.createFavorite(favoritesDTO);
-    return new ResponseEntity<>(HttpStatus.OK);
+  public ResponseEntity<FavoritesDTO> createFavorite(@RequestBody FavoritesDTO favoritesDTO) {
+    return new ResponseEntity<>(favoritesService.createFavorite(favoritesDTO),HttpStatus.OK);
   }
 
   @GetMapping("/list")
   @Operation(summary = "사용자 즐겨찾기 조회", description = "사용자의 즐겨찾기 전체 목록을 조회하는 API")
   @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(mediaType = "application/json"))
-  @Parameter(name = "userUuid", description = "사용자 UUID", example = "2025061406")
-  public ResponseEntity<List<FavoritesDTO>> getFavoritesList(@RequestParam String userUuid , @RequestParam FavoritesType favoritesType) {
-    return new ResponseEntity<>(favoritesService.getFavoritesList(userUuid,favoritesType), HttpStatus.OK);
+  @Parameter(name = "favoritesType", description = "즐겨찾기 타입", example = "RECIPE")
+  public ResponseEntity<List<FavoritesDTO>> getFavoritesList( @RequestParam FavoritesType favoritesType) {
+    return new ResponseEntity<>(favoritesService.getFavoritesList(favoritesType), HttpStatus.OK);
   }
 
   @DeleteMapping("/{id}")
