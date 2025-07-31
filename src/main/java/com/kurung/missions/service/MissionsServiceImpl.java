abda70d4fcc60d20060b58b62822eba058fa7b9b
chcyij9914 <chcyij9914@naver.com>
2025-07-31 16:33:30 +0900

    [FEAT] 미션뱃지

--- src/main/java/com/kurung/missions/service/MissionsServiceImpl.java
+++ src/main/java/com/kurung/missions/service/MissionsServiceImpl.java
@@ -17,7 +17,6 @@ import com.kurung.user.dto.UserDTO;
 import com.kurung.user.entity.UserEntity;
 import com.kurung.user.repository.UserRepository;
 import com.kurung.user.service.UserService;
-import jakarta.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.sql.Date;
